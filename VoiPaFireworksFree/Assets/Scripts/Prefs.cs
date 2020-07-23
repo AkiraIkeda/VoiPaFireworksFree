9f5dab1b1262a87721ba683febcf2a194a7675ef
AkiraIkeda <inothenxxxx@gmail.com>
2020-07-24 04:50:33 +0900

    20200724

---
+++ VoiPaFireworksFree/Assets/Scripts/Prefs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prefs{
+    public float cameraAngle;
+    public float cameraDepth;
+    public float cameraHeight;
+    public float livelyEffect;
+    public float shootingWidth;
+
+    public Prefs(float camera_angle, float camera_depth, float camera_height, float lively_effect, float shooting_width) {
+        cameraAngle = camera_angle;
+        cameraDepth = camera_depth;
+        cameraHeight = camera_height;
+        livelyEffect = lively_effect;
+        shootingWidth = shooting_width;
+    }
+}
